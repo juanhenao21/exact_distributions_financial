2cf0b498186cdcd87941754cf21389fed98a7e09
juanhenao21 <juanhenao21@gmail.com>
2021-03-20 18:54:11 +0100

    Add module to download the data

---
+++ project/download_data/download_data_main.py
@@ -0,0 +1,114 @@
+'''Portfolio optimization download data main module.
+
+The functions in the module download data from Yahoo! Finance for several years
+in several intervals.
+
+This script requires the following modules:
+    * typing
+    * calendar
+    * datetime
+    * pandas
+    * yfinance
+    * download_data_tools
+
+The module contains the following functions:
+    * portfolio_download_data - download data of a ticker.
+    * main - the main function of the script.
+
+.. moduleauthor:: Juan Camilo Henao Londono <www.github.com/juanhenao21>
+'''
+
+# -----------------------------------------------------------------------------
+# Modules
+
+from typing import List
+
+from calendar import monthrange
+from datetime import datetime as dt
+import pandas as pd  # type: ignore
+import yfinance as yf  # type: ignore
+
+import download_data_tools
+
+# -----------------------------------------------------------------------------
+
+
+def portfolio_download_data(tickers: List[str], dates: List[str],
+                            time_step: str) -> None:
+    """Downloads the prices of a ticker for an interval of years in a time
+       step.
+
+    :param tickers: list of the string abbreviation of the stocks to be
+     analyzed (i.e. ['AAPL', 'MSFT']).
+    :param dates: List of the interval of dates to be analyzed
+     (i.e. ['1980-01', '2020-12']).
+    :param time_step: time step of the data (i.e. '1m', '2m', '5m', ...).
+    :return: None -- The function saves the data in a file and does not return
+     a value.
+    """
+
+    try:
+        function_name: str = portfolio_download_data.__name__
+        download_data_tools \
+            .function_header_print_data(function_name, tickers, dates,
+                                        time_step)
+
+        init_year = int(dates[0].split('-')[0])
+        init_month = int(dates[0].split('-')[1])
+        fin_year = int(dates[1].split('-')[0])
+        fin_month = int(dates[1].split('-')[1])
+        last_day = monthrange(fin_year, fin_month)[1]
+
+        init_date: dt = dt(year=init_year, month=init_month, day=1)
+        fin_date: dt = dt(year=fin_year, month=fin_month, day=last_day)
+
+        # Not all the periods can be combined with the time steps.
+        raw_data: pd.DataFrame = \
+            yf.download(tickers=tickers, start=init_date, end=fin_date,
+                        interval=time_step)['Adj Close']
+        # Order DataFrame columns by sector
+        raw_data = raw_data[tickers]
+
+        if raw_data.isnull().values.any():
+            # Remove stocks that do not have data from the initial date
+            raw_data = raw_data.dropna(axis=1, thresh=len(raw_data) - 10) \
+                .fillna(method='ffill')
+
+        download_data_tools.save_data(raw_data, dates, time_step)
+
+    except AssertionError as error:
+        print('No data')
+        print(error)
+
+# -----------------------------------------------------------------------------
+
+
+def main() -> None:
+    """The main function of the script.
+
+    The main function extract, analyze and plot the data.
+
+    :return: None.
+    """
+
+    download_data_tools.initial_message()
+
+    # S&P 500 companies, initial year and time step
+    stocks: List[str] = download_data_tools.get_stocks(['all'])
+    dates: List[str] = ['1992-01', '2012-12']
+    time_step: str = '1d'
+
+    # Basic folders
+    download_data_tools.start_folders()
+
+    # Run analysis
+    # Download data
+    portfolio_download_data(stocks, dates, time_step)
+
+    print('Ay vamos!!!')
+
+# -----------------------------------------------------------------------------
+
+
+if __name__ == '__main__':
+    main()
---
+++ project/download_data/download_data_tools.py
@@ -0,0 +1,172 @@
+'''Portfolio optimization download data tools module.
+
+The functions in the module do small repetitive tasks, that are used along the
+whole implementation. These tools improve the way the tasks are standardized
+in the modules that use them.
+
+This script requires the following modules:
+    * os
+    * pickle
+    * typing
+    * pandas
+
+The module contains the following functions:
+    * save_data - saves the data downloaded.
+    * function_header_print_data - prints info about the function running.
+    * start_folders - creates folders to save data and plots.
+    * initial_message - prints the initial message with basic information.
+    * get_stocks - get the stocks of the S&P 500
+    * main - the main function of the script.
+
+.. moduleauthor:: Juan Camilo Henao Londono <www.github.com/juanhenao21>
+'''
+
+# -----------------------------------------------------------------------------
+# Modules
+
+import os
+import pickle
+from typing import List
+
+import pandas as pd  # type: ignore
+
+# -----------------------------------------------------------------------------
+
+
+def save_data(data: pd.DataFrame, dates: List[str], time_step: str) -> None:
+    """ Saves the data downloaded.
+
+    :param dates: List of the interval of dates to be analyzed
+     (i.e. ['1980-01', '2020-12']).
+    :param time_step: time step of the data (i.e. '1m', '2m', '5m', ...).
+    :return: None -- The function prints a message and does not return a value.
+    """
+
+    # Saving data
+
+    pickle.dump(data, open(f'../data/original_data/original_data_{dates[0]}'
+                           + f'_{dates[1]}_step_{time_step}.pickle', 'wb'))
+
+    print('Data Saved')
+    print()
+
+# -----------------------------------------------------------------------------
+
+
+def function_header_print_data(function_name: str, tickers: List[str],
+                               dates: List[str], time_step: str) -> None:
+    """Prints a header of a function that generates data when it is running.
+
+    :param function_name: name of the function that generates the data.
+    :param ticker: string of the abbreviation of the stock to be analyzed
+     (i.e. 'AAPL').
+    :param dates: List of the interval of dates to be analyzed
+     (i.e. ['1980-01', '2020-12']).
+    :param time_step: time step of the data (i.e. '1m', '2m', '5m', ...).
+    :return: None -- The function prints a message and does not return a value.
+    """
+
+    print('Portfolio Optimization')
+    print(function_name)
+
+    print(f'Downloading data for the tickers {tickers} in the interval time'
+          + f' from the years {dates[0]} to {dates[1]} in time steps of'
+          + f' {time_step}')
+    print()
+
+# -----------------------------------------------------------------------------
+
+
+def start_folders() -> None:
+    """Creates the initial folders to save the data and plots.
+
+    :return: None -- The function creates folders and does not return a value.
+    """
+    try:
+        os.mkdir('../data')
+        os.mkdir('../plot')
+        print('Folder to save data created')
+        print()
+
+    except FileExistsError as error:
+        print('Folder exists. The folder was not created')
+        print(error)
+        print()
+
+    try:
+        os.mkdir('../data/original_data')
+        print('Folder to save data created')
+        print()
+
+    except FileExistsError as error:
+        print('Folder exists. The folder was not created')
+        print(error)
+        print()
+
+# -----------------------------------------------------------------------------
+
+
+def initial_message() -> None:
+    """Prints the initial message with basic information.
+
+    :return: None -- The function prints a message and does not return a value.
+    """
+
+    print()
+    print('#####################')
+    print('Download tickers data')
+    print('#####################')
+    print('AG Guhr')
+    print('Faculty of Physics')
+    print('University of Duisburg-Essen')
+    print('Author: Juan Camilo Henao Londono')
+    print('More information in:')
+    print('* https://juanhenao21.github.io/')
+    print('* https://github.com/juanhenao21/portfolio_optimization')
+    # print('* https://forex-response_spread-year.readthedocs.io/en/latest/')
+    print()
+
+# -----------------------------------------------------------------------------
+
+
+def get_stocks(sectors: List[str]) -> List:
+    """Get the stocks from the S&P 500.
+
+    :param sectors: List of the sectors to download the data (i.e. ['all'],
+     ['Financials', 'Utilities']).
+    :return: List -- The function returns a list with stocks symbols.
+    """
+
+    data = pd.read_html(
+        'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
+
+    data = data.sort_values(by=['GICS Sector'], ignore_index=True)
+
+    data = data.groupby(by=['GICS Sector']) \
+        .apply(lambda x: x.sort_values(by=['Security'], ignore_index=True))
+
+    if sectors[0] == 'all':
+        stocks = list(data['Symbol'].values)
+    else:
+        stocks = []
+        for sector in sectors:
+            stocks.extend(data[data['GICS Sector'] == sector]['Symbol'].values)
+
+    return stocks
+
+# -----------------------------------------------------------------------------
+
+
+def main() -> None:
+    """The main function of the script.
+
+    The main function is used to test the functions in the script.
+
+    :return: None.
+    """
+
+# -----------------------------------------------------------------------------
+
+
+if __name__ == '__main__':
+    main()
