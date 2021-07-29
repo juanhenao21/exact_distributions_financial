cedcba02e70abc80ddaefa7c42c7ac4931f14fdd
Juan Henao <juanhenao21@gmail.com>
2021-07-29 12:01:26 +0200

    Update docstrings , add algebraic distribution

--- project/epochs/epochs_tools.py
+++ project/epochs/epochs_tools.py
@@ -1,4 +1,4 @@
-'''Local normalization tools module.
+'''Epochs tools module.
 
 The functions in the module do small repetitive tasks, that are used along the
 whole implementation. These tools improve the way the tasks are standardized
@@ -10,6 +10,7 @@ This script requires the following modules:
     * typing
     * matplotlib
     * numpy
+    * scipy
 
 The module contains the following functions:
     * save_data - saves computed data.
@@ -19,6 +20,7 @@ The module contains the following functions:
     * start_folders - creates folders to save data and plots.
     * initial_message - prints the initial message with basic information.
     * gaussian_distribution - compute gaussian distribution values.
+    * algebraic_distribution - compute algebraic distribution values.
     * main - the main function of the script.
 
 .. moduleauthor:: Juan Camilo Henao Londono <www.github.com/juanhenao21>
@@ -33,6 +35,8 @@ from typing import Any, List
 
 from matplotlib import pyplot as plt  # type: ignore
 import numpy as np  # type: ignore
+# Gamma function
+from scipy.special import gamma  # type: ignore
 
 # -----------------------------------------------------------------------------
 
@@ -41,15 +45,16 @@ def save_data(data: Any, function_name: str, dates: List[str], time_step: str,
               window: str) -> None:
     """Saves computed data in pickle files.
 
-    Saves the data generated in the functions of the
-    local_normalization_analysis module in pickle files.
+    Saves the data generated in the functions of the epochs_analysis module in
+    pickle files.
 
     :param data: data to be saved. The data can be of different types.
     :param function_name: name of the function that generates the plot.
     :param dates: List of the interval of dates to be analyzed
-     (i.e. ['1980-01', '2020-12']).
-    :param time_step: time step of the data (i.e. '1m', '2m', '5m', ...).
-    :param window: window time to compute the volatility (i.e. '60').
+     (i.e. ['1980-01-01', '2020-12-31']).
+    :param time_step: time step of the data (i.e. '1m', '1h', '1d', '1wk',
+     '1mo').
+    :param window: window time to use in the computation (i.e. '25').
     :return: None -- The function saves the data in a file and does not return
      a value.
     """
@@ -57,8 +62,8 @@ def save_data(data: Any, function_name: str, dates: List[str], time_step: str,
     # Saving data
 
     pickle.dump(data, open(
-        f'../data/local_normalization/{function_name}_{dates[0]}_{dates[1]}'
-                + f'_step_{time_step}_win_{window}.pickle', 'wb'), protocol=4)
+        f'../data/epochs/{function_name}_{dates[0]}_{dates[1]}_step'
+                + f'_{time_step}_win_{window}.pickle', 'wb'), protocol=4)
 
     print('Data Saved')
     print()
@@ -70,23 +75,24 @@ def save_plot(figure: plt.Figure, function_name: str, dates: List[str],
               time_step: str, window: str) -> None:
     """Saves plot in png files.
 
-    Saves the plot generated in the functions of the
-    local_normalization_analysis module in png files.
+    Saves the plot generated in the functions of the epochs_analysis module in
+    png files.
 
     :param figure: figure object that is going to be save.
     :param function_name: name of the function that generates the plot.
     :param dates: List of the interval of dates to be analyzed
-     (i.e. ['1980-01', '2020-12']).
-    :param time_step: time step of the data (i.e. '1m', '2m', '5m', ...).
-    :param window: window time to compute the volatility (i.e. '60').
+     (i.e. ['1980-01-01', '2020-12-31']).
+    :param time_step: time step of the data (i.e. '1m', '1h', '1d', '1wk',
+     '1mo').
+    :param window: window time to compute the volatility (i.e. '25').
     :return: None -- The function save the plot in a file and does not return
      a value.
     """
 
     # Saving plot data
 
-    figure.savefig(f'../plot/local_normalization/{function_name}_{dates[0]}'
-                   + f'_{dates[1]}_step_{time_step}_win_{window}.png')
+    figure.savefig(f'../plot/epochs/{function_name}_{dates[0]}_{dates[1]}'
+                   + f'_step_{time_step}_win_{window}.png')
 
     print('Plot Saved')
     print()
@@ -100,9 +106,10 @@ def function_header_print_data(function_name: str, dates: List[str],
 
     :param function_name: name of the function that generates the data.
     :param dates: List of the interval of dates to be analyzed
-     (i.e. ['1980-01', '2020-12']).
-    :param time_step: time step of the data (i.e. '1m', '2m', '5m', ...).
-    :param window: window time to compute the volatility (i.e. '60').
+     (i.e. ['1980-01-01', '2020-12-31']).
+    :param time_step: time step of the data (i.e. '1m', '1h', '1d', '1wk',
+     '1mo').
+    :param window: window time to compute the volatility (i.e. '25').
     :return: None -- The function prints a message and does not return a
      value.
     """
@@ -124,9 +131,10 @@ def function_header_print_plot(function_name: str, dates: List[str],
 
     :param function_name: name of the function that generates the data.
     :param dates: List of the interval of dates to be analyzed
-     (i.e. ['1980-01', '2020-12']).
-    :param time_step: time step of the data (i.e. '1m', '2m', '5m', ...).
-    :param window: window time to compute the volatility (i.e. '60').
+     (i.e. ['1980-01-01', '2020-12-31']).
+    :param time_step: time step of the data (i.e. '1m', '1h', '1d', '1wk',
+     '1mo').
+    :param window: window time to compute the volatility (i.e. '25').
     :return: None -- The function prints a message and does not return a
      value.
     """
@@ -149,8 +157,8 @@ def start_folders() -> None:
     """
 
     try:
-        os.mkdir(f'../data/local_normalization')
-        os.mkdir(f'../plot/local_normalization')
+        os.mkdir(f'../data/epochs')
+        os.mkdir(f'../plot/epochs')
         print('Folder to save data created')
         print()
 
@@ -169,9 +177,9 @@ def initial_message() -> None:
     """
 
     print()
-    print('###################')
-    print('Exact Distributions')
-    print('###################')
+    print('######')
+    print('Epochs')
+    print('######')
     print('AG Guhr')
     print('Faculty of Physics')
     print('University of Duisburg-Essen')
@@ -191,12 +199,31 @@ def gaussian_distribution(mean: float, variance: float,
 
         :param mean: mean of the Gaussian distribution.
         :param variance: variance of the Gaussian distribution.
-        :param x: array of the values to compute the Gaussian
+        :param x_values: array of the values to compute the Gaussian
          distribution
     """
 
     return (1 / (2 * np.pi * variance) ** 0.5) \
         * np.exp(-((x_values - mean) ** 2) / (2 * variance))
+
+# -----------------------------------------------------------------------------
+
+
+def algebraic_distribution(variance: float, K: int, l: int, m: int,
+                           x_values: np.ndarray) -> np.ndarray:
+    """Compute the algebraic distribution values.
+
+        :param variance: variance of the algebraic distribution.
+        :param K: number of companies analyzed.
+        :param l: shape parameter.
+        :param m: shape parameter.
+        :param x_values: array of the values to compute the Gaussian
+         distribution
+    """
+
+    return (np.sqrt(2 / m)) ** K * (gamma(l) / gamma(l - K / 2)) \
+        (1 / np.sqrt(2 * np.pi * variance)) \
+        * (1 / (1 + (1 / m) * x_values * x_values * (1 / variance)))
 
 # -----------------------------------------------------------------------------
 
