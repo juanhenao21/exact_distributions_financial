2862d84034558ac761c432ae9f9d333e3c177074
juanhenao21 <juanhenao21@gmail.com>
2021-10-11 17:49:28 +0200

    Check with long epochs window

--- project/epochs_sim/epochs_sim_main.py
+++ project/epochs_sim/epochs_sim_main.py
@@ -37,11 +37,11 @@ def data_plot_generator() -> None:
     """
 
     # Simulate the aggregated returns for different epochs lenghts
-    for epochs_len in [10, 25, 40, 55]:
+    for epochs_len in [500]: #[10, 25, 40, 55]:
         returns_pairs = epochs_sim_analysis \
-            .epochs_sim_agg_returns_market_data(0.3, 2, 50, 40, epochs_len)
+            .epochs_sim_agg_returns_market_data(0.3, 2, 50, 40, epochs_len, kind='algebraic')
         epochs_sim_plot \
-            .epochs_sim_agg_returns_market_plot(returns_pairs, epochs_len)
+            .epochs_sim_agg_returns_market_plot(returns_pairs, epochs_len, 50, kind='algebraic')
 
     # for epochs_len in [10, 25, 100]:
     #     returns_market = epochs_sim_analysis \
